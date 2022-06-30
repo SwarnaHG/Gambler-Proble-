5f3b9cd7bd9457e40c6ba99f6299493eeb9a8024
SwarnaHG <swarnahg18@gmail.com>
2022-06-30 19:22:06 +0530

    uc2 of gambler problem

--- src/Gambler.java
+++ src/Gambler.java
@@ -1,18 +1,28 @@
+public class Gambler{
+    /**
+     * initialize stack is 100
+     * initialize bet is 1
+     * checking wheather win the bet or lose
+     * if win then bet will increase by 1
+     * if decrease then bet will decrease by 1
+     *
+     */
+    static int EverydayStack = 100;
+    static int BetStack = 1;
 
-    package gamblingGame;
-    public class Gambler {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambling");
+        System.out.println("Every Day Stack is " + EverydayStack);
+        System.out.println("Betting price is " + BetStack);
 
+        double Bet = Math.random();
+        if (Bet < 0.5) {
 
-        public static void main(String[] args) {
-            System.out.println("Welcome Gambler");
-
-            int EveryDayStack = 100;
-            int BetForGame = 1;
-
-            System.out.println("Every Day Stack is " + EveryDayStack);
-            System.out.println("Bet Price: " + BetForGame);
-
+            System.out.println("win the bet" + (EverydayStack += BetStack));
+        } else {
+            System.out.println("Floss the bet " + (EverydayStack -= BetStack));
         }
 
     }
 
+}
