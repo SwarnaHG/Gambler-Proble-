120ac1a877fa9a55b1d40b424cdda7287506733d
SwarnaHG <swarnahg18@gmail.com>
2022-06-30 19:10:00 +0530

    Gambler problem usecase 1

---
+++ src/Gambler.java
@@ -0,0 +1,18 @@
+
+    package gamblingGame;
+    public class Gambler {
+
+
+        public static void main(String[] args) {
+            System.out.println("Welcome Gambler");
+
+            int EveryDayStack = 100;
+            int BetForGame = 1;
+
+            System.out.println("Every Day Stack is " + EveryDayStack);
+            System.out.println("Bet Price: " + BetForGame);
+
+        }
+
+    }
+
