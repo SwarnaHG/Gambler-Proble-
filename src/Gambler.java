e7fb6065ae8bb8dd04c767e652396be7d89a462c
SwarnaHG <swarnahg18@gmail.com>
2022-06-30 19:33:30 +0530

    Uc4 of gambler problem

--- src/Gambler.java
+++ src/Gambler.java
@@ -1,31 +1,40 @@
-
-
-import java.util.Random;
-
 public class Gambler {
+    /**
+     * initialize stack is 100 initialize bet is 1 claculating for 20 days if win
+     * the stack =stack+50 if loss stack= stack-50 final prining win after 20 days
+     * wheather it is loss or profit
+     *
+     */
     static int EverydayStack = 100;
     static int BetStack = 1;
 
     public static void main(String[] args) {
         int StackIncrease = 150;
         int StackDecrease = 50;
-        int win =0;
+        int win = 0;
         System.out.println("Welcome To Gambling");
         System.out.println("Every Day Stack is " + EverydayStack);
         System.out.println("Betting price is " + BetStack);
+        for (int day = 1; day <= 20; day++) {
 
+            while (EverydayStack > StackDecrease && EverydayStack < StackIncrease) {
+                double rand = Math.random();
 
-
-        while(EverydayStack > StackDecrease && EverydayStack < StackIncrease) {
-            double rand = Math.random();
-
-            if(rand < 0.5) {
-                //  System.out.println("Bet Win");
-                System.out.println("Win and amount is "+(EverydayStack +=BetStack));
-            }else {
-                //  System.out.println("Bet Loss");
-                System.out.println("Loss and amount is "+(EverydayStack -= BetStack));
+                if (rand < 0.5) {
+                    System.out.println("win the bet and stack is " + (EverydayStack += BetStack));
+                    win = win + 50;
+                    break;
+                } else {
+                    System.out.println("Loss the bet and stack is " + (EverydayStack -= BetStack));
+                    win = win - 50;
+                    break;
+                }
             }
+
+            System.out.println("winning amount is " + win);
+
         }
+        System.out.println(" final winning amount is " + win);
+
     }
 }
